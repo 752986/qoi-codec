c830503bfdeb5d4b15045c85cc5fff2aaa93fc68
Dec <112513827+752986@users.noreply.github.com>
2023-05-01 14:31:01 -0600

    Command line arguments test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
+use std::env::args;
+
 mod image;
 
 fn main() {
-    println!("Hello, world!");
+    let mut arguments: Vec<String> = args().collect();
+    arguments.remove(0);
+
+    let infile: String;
+    let outfile: String;
+    match arguments.len() {
+        1 => {
+            infile = arguments[0].clone();
+            outfile = infile.clone();
+        }
+        2 => {
+            infile = arguments[0].clone();
+            outfile = arguments[1].clone();
+        }
+        _ =>  {
+            println!("Invalid number of arguments \nUsage: qoi.exe <input> [output]");
+            return;
+        }
+    }
+    println!("{infile}, {outfile}");
 }
