7c50257c1634e426062a732ec611e1089a96cb7b
Dec <112513827+752986@users.noreply.github.com>
2023-05-05 14:39:42 -0600

    Ad png write method

--- src/my_image.rs
+++ src/my_image.rs
@@ -4,18 +4,18 @@ use std::io;
 
 /// Contains the data of an image.
 pub struct Image {
-	/// The width of the image.
+    /// The width of the image.
     pub width: usize,
-	/// The height of the image.
+    /// The height of the image.
     pub height: usize,
-	/// The number of channels per pixel (3 for RGB, 4 for RGBA).
+    /// The number of channels per pixel (3 for RGB, 4 for RGBA).
     stride: usize,
-	/// The underlying pixel data of the image.
+    /// The underlying pixel data of the image.
     pixels: Vec<u8>,
 }
 
 impl Image {
-	/// Creates a new empty image. `stride` is the number of channels per pixel (3 for RGB, 4 for RGBA).
+    /// Creates a new empty image. `stride` is the number of channels per pixel (3 for RGB, 4 for RGBA).
     pub fn new(width: usize, height: usize, stride: usize) -> Self {
         return Self {
             width,
@@ -42,11 +42,11 @@ impl Image {
         use image::io::Reader;
         let image = Reader::open(filepath)?.decode().unwrap();
         let samples = image.as_flat_samples_u8().unwrap();
-    
+
         let pixels = samples.as_slice().to_owned();
         let width = samples.layout.width as usize;
         let stride = samples.layout.width_stride;
-    
+
         return Ok(Image::from(pixels, width, stride));
     }
 
@@ -55,22 +55,38 @@ impl Image {
         use image::io::Reader;
         let image = Reader::open(filepath)?.decode().unwrap();
         let samples = image.as_flat_samples_u8().unwrap();
-    
+
         let pixels = samples.as_slice().to_owned();
         let width = samples.layout.width as usize;
         let stride = samples.layout.width_stride;
-    
+
         return Ok(Image::from(pixels, width, stride));
     }
 
-	/// Gets a reference to the bytes of a pixel. The length of the slice will be equal to the stride of the image.
+    pub fn write_png(&self, filepath: &str) -> bool {
+        return image::save_buffer_with_format(
+            filepath,
+            &self.pixels,
+            self.width as u32,
+            self.height as u32,
+            if self.stride == 3 {
+                image::ColorType::Rgb8
+            } else {
+                image::ColorType::Rgba8
+            },
+            image::ImageFormat::Png,
+        )
+        .is_ok();
+    }
+
+    /// Gets a reference to the bytes of a pixel. The length of the slice will be equal to the stride of the image.
     pub fn get_pixel(&self, x: usize, y: usize) -> &[u8] {
         let index = ((y * self.width) + x) * self.stride;
 
         return &self.pixels[index..index + self.stride];
     }
 
-	/// Gets a mutable reference to the bytes of a pixel. The length of the slice will be equal to the stride of the image.
+    /// Gets a mutable reference to the bytes of a pixel. The length of the slice will be equal to the stride of the image.
     pub fn get_pixel_mut(&mut self, x: usize, y: usize) -> &mut [u8] {
         let index = ((y * self.width) + x) * self.stride;
 
